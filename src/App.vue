9735ad58141769ff851bc07972c4945731d3daba
Frederic <frederic.becker@mail.de>
2022-02-08 20:24:27 +0100

    reset: to mental rotation experiment

--- src/App.vue
+++ src/App.vue
@@ -1,142 +1,109 @@
 <template>
-  <Experiment title="_magpie demo">
+  <Experiment title="_magpie mental-rotation">
     <InstructionScreen :title="'Welcome'">
-      This is a sample introduction screen.
-      <br />
-      <br />
-      This screen welcomes the participant and gives general information about
-      the experiment.
-      <br />
-      <br />
-      This mock up experiment is a showcase of the functionality of magpie.
+      Welcome to this short experiment (~10 minutes) on visual cognition. Thank you for participating!
+      <br />  <br />
+      Click next to learn more about the experimental task.
     </InstructionScreen>
 
     <InstructionScreen :title="'General Instructions'">
-      This is a sample instructions view.
+      <h2>Task:</h2>
+      In the following 11 trials you will be presented two images of gemoetric cubes.
+      Your task is to decide whether both cubes share the same shape or not.
       <br />
       <br />
-      First you will go through two practice trials. The practice trial view
-      uses magpie's forced choice trial input.
+      Please press button  <b>f</b> on your keyboard if you think that the cubes have <b>{{keys[0]}}</b> shapes.
+      <br />
+      Please press button  <b>j</b> on your keyboard if you think that the cubes have <b>{{keys[1]}}</b> shapes.
+
+      <br />
+      <br />
+      It is important that you try to response as fast and as correct as possible!
+
+      <br />
+      Good luck!
     </InstructionScreen>
 
     <!-- Here we create screens in a loop for every entry in forced_choice -->
-    <template v-for="(forced_choice_task, i) of forced_choice">
-      <ForcedChoiceScreen
-        :key="'forcedchoice-' + i"
-        :progress="i / forced_choice.length"
-        :question="forced_choice_task.question"
-        :options="[forced_choice_task.option1, forced_choice_task.option2]"
+    <template v-for="(training_trial, i) of training_trials">
+      <KeypressScreen
+        :keys="{'f': keys[0], 'j': keys[1]}"
+        :progress="i / training_trials.length"
+        :question="training_trial.question"
+        :options="[training_trial.option1, training_trial.option2]"
+        :fixationTime= "Math.floor(Math.random() * 1500 + 500)"
+        :responseTime="7500"
+        :feedbackTime="800"
       >
         <template #stimulus>
-          <img :src="forced_choice_task.picture" />
+          <img :src="training_trial.picture" />
+          <Record :data="{
+              image: training_trial.picture,
+              angle: training_trial.angle,
+              expected: training_trial.expected,
+              item: training_trial.item,
+              trial_type: 'training',
+              trial_number: i
+            }" />
         </template>
-      </ForcedChoiceScreen>
-    </template>
 
-    <template v-for="(dropdown_task, i) in multi_dropdown">
-      <CompletionScreen
-        :key="'multidropdown-' + i"
-        :progress="i / multi_dropdown.length"
-        :text="
-          dropdown_task.sentence_chunk_1 +
-          ' %s ' +
-          dropdown_task.sentence_chunk_2 +
-          ' %s ' +
-          dropdown_task.sentence_chunk_3
-        "
-        :options="[
-          dropdown_task.choice_options_1.split('|'),
-          dropdown_task.choice_options_2.split('|')
-        ]"
-      />
-    </template>
-
-    <template v-for="i in range(0, sentenceChoice.length, 2)">
-      <template v-for="(sentenceChoice_task, j) in sentenceChoice.slice(i, 2)">
-        <ForcedChoiceScreen
-          :key="'sentenceChoice-' + i + '' + j"
-          :question="sentenceChoice_task.question"
-          :options="[sentenceChoice_task.option1, sentenceChoice_task.option2]"
-        >
-          <template #stimulus>
-            <img :src="sentenceChoice_task.picture" />
-          </template>
-        </ForcedChoiceScreen>
-      </template>
-      <template v-for="(imageSelection_task, j) in imageSelection.slice(i, 2)">
-        <ImageSelectionScreen
-          :key="'sentenceChoice-' + i + '' + j"
-          :question="imageSelection_task.question"
-          :options="[
-            {
-              label: imageSelection_task.option1,
-              src: imageSelection_task.picture1
-            },
-            {
-              label: imageSelection_task.option2,
-              src: imageSelection_task.picture2
-            }
-          ]"
-        />
-      </template>
-    </template>
-
-    <template v-for="(rating_task, i) in sliderRating">
-      <SliderScreen
-        :key="'sliderRating-' + i"
-        :pause-time="500"
-        :stimulus-time="1500"
-        :question="rating_task.question"
-        :option-left="rating_task.optionLeft"
-        :option-right="rating_task.optionRight"
-      >
-        <template #stimulus>
-          <img :src="rating_task.picture" alt="" />
+        <template #feedback>
+          <p v-if="!$magpie.measurements.hasOwnProperty('response')">Faster!</p>
+          <p v-else-if="$magpie.measurements.response == $magpie.measurements.expected"> Correct! </p>
+          <p v-else> Wrong!</p>
         </template>
-      </SliderScreen>
+      </KeypressScreen>
     </template>
-
-    <!--
-
-      Comment this in, to try out interactive components like the Chat component.
-
-      <ConnectInteractiveScreen />
-
-      <Screen>
-          <Chat :messages.sync="$magpie.measurements.messages"></Chat>
-          <button @click="$magpie.saveAndNextScreen()">Next</button>
-      </Screen>
-
-      -->
 
     <PostTestScreen />
 
     <!-- While developing your experiment, using the DebugResults screen is fine,
-      once you're going live, you can use the <SubmitResults> screen to automatically send your experimental data to the server. -->
-    <DebugResultsScreen />
+      once you're going live, you can use the <SubmitResults> screen to automatically send your experimental data to the server.
+
+      <template #feedback>
+        <p v-if="!$magpie.measurements.hasOwnProperty('response')">Faster!</p>
+        <p v-else-if="!$magpie.measurements.hasOwnProperty('response')"> Wrong </p>
+        <p v-else> Correct </p>
+      </template>
+
+      mounted() { this.$magpie.addExpData({
+          same_key : same_key });
+      }
+
+    -->
+    <SubmitResultsScreen/>
   </Experiment>
 </template>
 
 <script>
+
 // Load data from csv files as javascript arrays with objects
-import forced_choice from '../trials/forced_choice.csv';
-import multi_dropdown from '../trials/multi_dropdown.csv';
-import sentenceChoice from '../trials/sentence_choice.csv';
+import main_trials from '../trials/mr_main_trials.csv';
+import training_trials from '../trials/mr_training_trials.csv';
+
 import _ from 'lodash';
 
 export default {
   name: 'App',
   data() {
+
     return {
-      forced_choice,
-      multi_dropdown,
-      sentenceChoice,
+      main_trials: _.shuffle(main_trials),
+      training_trials: _.shuffle(training_trials),
       imageSelection: _.shuffle(imageSelection),
       sliderRating,
+      keys: _.shuffle(['same', 'different']),
 
       // Expose lodash.range to template above
       range: _.range
     };
+  },
+
+  mounted() {
+    // store same key
+    let same_key = this._data.keys[0] == 'same' ? 'f' : 'j';
+    this.$magpie.addExpData({
+      same_key : same_key  });
   }
 };
 
