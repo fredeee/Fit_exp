d0fe3c33e891ce45000418aaf5cad8881723e6ae
Frederic <frederic.becker@mail.de>
2022-02-21 18:25:17 +0100

    feat: add stimuli

--- exp/src/App.vue
+++ exp/src/App.vue
@@ -1,13 +1,17 @@
 <template>
   <Experiment title="_magpie mental-rotation">
     <InstructionScreen :title="'Welcome'">
-      Welcome to this short psychology experiment (~10 minutes). Thank you for participating!
+      Welcome and thank you for participating!
      <br/> <br />
-      This study is conducted by a student seminar at the Universität Tübingen.
-      We only collect anonymous data such as response times. You will not be compensated for your time, except for a sincere thank you. You can stop the experiment at any point if you feel so.
+
+      In this short psychology experiment (~10 minutes) we will collect anonymous data only
+      (e.g., accuracy of response and response times). The study is conducted by a student seminar at the University of Tuebingen.
+      You will not be compensated for your time, except for a sincere thank you for taking an active part in science.
+
+      However, if you do not feel like participating you can stop the experiment at any time by closing the tab.
       If you have further questions feel free to contact us via <a href="mailto:frederic.becker@student.uni-tuebingen.de">email</a>.
       <br />  <br />
-      Click next to learn more about the experimental task.
+      Click next to learn more about your task.
     </InstructionScreen>
 
     <InstructionScreen :title="'Task'">
@@ -26,13 +30,16 @@
     </InstructionScreen>
 
     <InstructionScreen :title="'Procedure'">
-      The experiment consists of two task blocks. Before each block we will inform you about the specific letter you should look for. <b>This letter only changes between blocks</b>.
+      The experiment consists of two task blocks. Prior to each block we will inform you about the specific target letter you are supposed to look out for.
+      <b>The target letter only changes between blocks not between trials!</b>.
+      <br />
+      After each block you will get the chance to take a short pause if you like before you continue with the next block.
       <br />
     </InstructionScreen>
 
     <InstructionScreen :title="'Calibration'">
       To ensure that the trials are presented correctly, it is necessary to shortly calibrate the size of your browser window.
-      Please adjust the size of your browser window (zoom option) such that the displayed form roughly matches the size of an actual credit card or ID card.
+      Please adjust the size of your browser window (zoom option, press ctrl/strg and ‘+’ or ‘-‘) such that the displayed form roughly matches the size of an actual credit card or ID card.
       <br/>  <br/>
       <div style="width: 375px; height: 230px; border-radius: 10px; background-color: grey; margin-left: auto; margin-right: auto;"></div>
       <br/>
@@ -50,7 +57,7 @@
     <template v-for="(block, i) of blocks">
       <InstructionScreen :title="'New Block'">
         In the upcoming trials your task to search for
-        <span v-if="block[0].condition === 'disjunction'">
+        <span v-if="block[0].condition === 'feature'">
           an <b>S or any <span style='color: blue'> blue</span> letter.</b>
         </span>
         <span v-else>
@@ -95,32 +102,33 @@
       once you're going live, you can use the <SubmitResults> screen to automatically send your experimental data to the server.
     -->
 
-    <SubmitResultsScreen/>
-    <!-- DebugResultsScreen/ -->
+    <!-- SubmitResultsScreen/ -->
+    <DebugResultsScreen/>
   </Experiment>
 </template>
 
 <script>
 
 // Load data from csv files as javascript arrays with objects
-import conjunction_trials from '../trials/conjunction_trials.csv';
-import disjunction_trials from '../trials/disjunction_trials.csv';
+import feature_trials1 from '../trials/feature1.csv';
+import feature_trials2 from '../trials/feature2.csv';
+
+import disjunction_trials1 from '../trials/disjunction1.csv';
+import disjunction_trials2 from '../trials/disjunction2.csv';
+
 import _ from 'lodash';
 
 // block randomization
-let blockorder = _.shuffle(['CDDCCD', 'DCCDDC'])[0];
-//let blockorder = _.shuffle(['C', 'D'])[0];
+let blockorder = _.shuffle(['CDCD', 'DCDC'])[0];
 
-// double trials from 64 to 128, suich that each apears twice in a block
-let conjunction_block = conjunction_trials.concat(conjunction_trials);
-let disjunction_block = disjunction_trials.concat(disjunction_trials);
+// randomize within conditions
+let conjunction_blocks = _.shuffle([feature_trials1, feature_trials2]);
+let disjunction_blocks = _.shuffle([disjunction_trials1, disjunction_trials2]);
 
-// create blocks accoridng to block order
+// create blocks according to block order
 let blocks = [];
-let target_names = [];
 for(const block of blockorder){
-  blocks.push(block == 'C' ? _.shuffle(conjunction_block) : _.shuffle(disjunction_block));
-  target_names.push()
+  blocks.push(block == 'C' ? _.shuffle(conjunction_blocks.pop()) : _.shuffle(disjunction_blocks.pop()));
 }
 
 export default {
@@ -146,6 +154,8 @@ export default {
           if(Object.keys(trial_data).length > 0){
             let last_key = Object.keys(trial_data).sort().reverse()[0];
             let last_trial = trial_data[last_key];
+
+            console.log(last_trial)
             return last_trial[0].response_time;
 
           } else {
