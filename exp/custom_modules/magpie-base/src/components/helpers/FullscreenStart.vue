759d5fb2a8c8ff258c1876a5b3036ca250309833
Frederic <frederic.becker@mail.de>
2022-02-25 13:04:56 +0100

    add: customized magpie plugins

---
+++ exp/custom_modules/magpie-base/src/components/helpers/FullscreenStart.vue
@@ -0,0 +1,15 @@
+<docs>
+This component asks the user to enable full screen.
+</docs>
+<template><span /></template>
+<script>
+export default {
+  name: 'FullscreenStart',
+  mounted() {
+    document.documentElement.requestFullscreen();
+    $magpie.nextScreen()
+  }
+};
+</script>
+
+<style scoped></style>
